da60f327ce985fcc32a6173642d92b9bd6301fe8
Igor <61108251+zEphyrUnity@users.noreply.github.com>
2020-03-04 23:48:06 +0300

    Add files via upload

---
+++ Task2.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Lesson6
+{
+    class Program
+    {
+        public delegate double MathFunc(double x);
+
+
+        public static double Quadratic(double x)
+        {
+            return Math.Sqrt(x);
+        }
+
+        public static double SinusX(double x)
+        {
+            return Math.Sin(x);
+        }
+
+        public static double SomeF(double x)
+        {
+            return x * x - 50 * x + 10;
+        }
+
+        public static void SaveFunc(string fileName, int a, int b, double h, List<MathFunc> F, byte funcNumber)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+
+            double x = a;
+            while (x <= b)
+            {
+                if (funcNumber == 1)
+                {
+                    bw.Write(F[0](x));
+                    x += h;
+                }
+
+                if (funcNumber == 2)
+                {
+                    bw.Write(F[1](x));
+                    x += h;
+                }
+
+                if (funcNumber == 3)
+                {
+                    bw.Write(F[2](x));
+                    x += h;
+                }
+            }
+
+            bw.Close();
+            fs.Close();
+        }
+        public static double[] Load(string fileName, ref double minValue)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader bw = new BinaryReader(fs);
+
+            minValue = double.MaxValue;
+            double[] binaryArray = new double[fs.Length / sizeof(double)];
+            double d;
+
+            for (int i = 0; i < fs.Length / sizeof(double); i++)
+            {
+                //Считываем значение и переходим к следующему
+                binaryArray[i] = d = bw.ReadDouble();
+                if (d < minValue) minValue = d;
+            }
+
+            bw.Close();
+            fs.Close();
+
+            return binaryArray;
+        }
+
+        static void Main()
+        { 
+            MathFunc Func1 = Quadratic;
+            MathFunc Func2 = SinusX;
+            MathFunc Func3 = SomeF;
+            List<MathFunc> FunctionList = new List<MathFunc>();
+            
+            FunctionList.Add(Func1);
+            FunctionList.Add(Func2);
+            FunctionList.Add(Func3);
+
+            byte funcNumber = 1;
+            int a = 0;
+            int b = 0;
+            byte step = 0;
+
+            //Меню----------------------------------------------
+            bool condition = false;
+            do
+            {
+                if(step == 0)
+                {
+                    Console.WriteLine("Выберите функцию для нахождения ее минимума: \"1\" - Math.Sqrt(x) | \"2\" - Math.Sin(x) | \"3\" - SomeF(x)");
+                    if (!Byte.TryParse(Console.ReadLine(), out funcNumber) || (funcNumber > 3 || funcNumber < 1))
+                    {
+                        Console.Clear();
+                        continue;
+                    }
+                    else { step++; }
+                }
+
+                if (step == 1)
+                {
+                    Console.WriteLine("Укажите на каком отрезке находить минимум.");
+                    Console.WriteLine("Введите \"a\".");
+                    if (!Int32.TryParse(Console.ReadLine(), out a))
+                    {
+                        Console.WriteLine("Введите число");
+                        Console.Clear();
+                        continue;
+                    }
+                    else { step++; }
+                }
+
+                if (step == 2)
+                {
+                    Console.WriteLine("Укажите на каком отрезке находить минимум.");
+                    Console.WriteLine("Введите \"b\".");
+                    if (!Int32.TryParse(Console.ReadLine(), out b))
+                    {
+                        Console.WriteLine("Введите число");
+                        Console.Clear();
+                        continue;
+                    }
+                }
+
+                SaveFunc("data.bin", a, b, 0.5, FunctionList, funcNumber);
+                condition = true;
+            }
+            while (!condition);
+            //---------------------------------------------------
+
+            double minValue = 0;
+            var binaryArray = Load("data.bin", ref minValue);
+            
+            //Вывод минимум функции
+            Console.WriteLine("Минимум функции {0:f2}", minValue);
+
+            //Вывод массива значений функции
+            foreach (double number in binaryArray)
+            {
+                if (number != 0)
+                {
+                    Console.Write("{0:f2} ", number);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Task3.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+
+class Program
+{
+    public struct Students : IComparable
+    {
+        public string name;
+        public int age;
+        public int course;
+
+        public int CompareTo(object obj1)
+        {
+            Students object1;
+            object1 = (Students)obj1;
+
+            return age.CompareTo(object1.age);
+        }
+    }
+
+    static int CompareTwo(Students person1, Students person2)
+    {
+        int check = 0;
+
+        if(person1.course < person2.course) check = -1;
+
+        if(person1.course > person2.course) check = 1;
+
+        if(person1.course == person2.course)
+        {
+            if (person1.age < person2.age) check = -1;
+
+            if (person1.age > person2.age) check = 1;
+        }
+
+        return check;
+    }
+
+    static void Main(string[] args)
+    {
+        int bakalavr = 0;
+        int magistr = 0;
+        int seniorCourse = 0;
+        int[] statistics = new int[6];
+        
+        // Запомним время в начале обработки данных
+        DateTime dt = DateTime.Now;
+        StreamReader sr = new StreamReader("..\\..\\students_1.csv");
+
+        Students st = new Students();
+        List<Students> students = new List<Students>();
+
+        while (!sr.EndOfStream)
+        {
+            try
+            {
+                string[] s = sr.ReadLine().Split(';');
+
+                students.Add(st);
+                st.name = s[1] + " " + s[0];
+                st.age = Int32.Parse(s[5]);
+                st.course = Int32.Parse(s[6]);
+
+                if (int.Parse(s[6]) < 5) bakalavr++; else magistr++;
+                if (Int32.Parse(s[6]) >= 5) seniorCourse++;
+
+                switch (Int32.Parse(s[6]))
+                {
+                    case 1: if (Int32.Parse(s[5]) >= 18 && Int32.Parse(s[5]) <= 20) statistics[0]++;
+                        break;
+                    case 2: if (Int32.Parse(s[5]) >= 18 && Int32.Parse(s[5]) <= 20) statistics[1]++;
+                        break;
+                    case 3: if (Int32.Parse(s[5]) >= 18 && Int32.Parse(s[5]) <= 20) statistics[2]++;
+                        break;
+                    case 4: if (Int32.Parse(s[5]) >= 18 && Int32.Parse(s[5]) <= 20) statistics[3]++;
+                        break;
+                    case 5: if (Int32.Parse(s[5]) >= 18 && Int32.Parse(s[5]) <= 20) statistics[4]++;
+                        break;
+                    case 6: if (Int32.Parse(s[5]) >= 18 && Int32.Parse(s[5]) <= 20) statistics[5]++;
+                        break;
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        sr.Close();
+
+        Console.WriteLine("Всего студентов:{0}", students.Count);
+        Console.WriteLine("Магистров:{0}", magistr);
+        Console.WriteLine("Бакалавров:{0}", bakalavr);
+        Console.WriteLine($"Старшекурсников:{seniorCourse}");
+
+        //foreach (var v in list) Console.WriteLine(v);
+        for(int i = 0; i < statistics.Length; i++)
+        {
+            Console.WriteLine($"Студентов {i+1} курса, в возрасте от 18 до 20 лет: {statistics[i]}");
+        }
+
+        //Простая сортировка по одному полю
+        //for (int i = 0; i < students.Count - 1; i++)
+        //{
+        //    for (int j = i + 1; j < students.Count; j++)
+        //    {
+        //        if (students[i].course > students[j].age)
+        //        {
+        //            Students buffer = students[i];
+        //            students[i] = students[j];
+        //            students[j] = buffer;
+        //        }
+        //    }
+        //}
+
+        //Сортировка по одному полю, реализация интерфейса IComparable, метод CompareTo
+        //students.Sort();      
+        
+        //Сортировка по двум полям, метод CompareTwo
+        students.Sort(CompareTwo);
+
+        foreach (Students person in students)
+        {
+            Console.WriteLine($"{person.course} {person.age} {person.name}");
+        }
+
+        // Вычислим время обработки данных
+        Console.WriteLine(DateTime.Now - dt);
+        Console.ReadKey();
+    }
+}
+
